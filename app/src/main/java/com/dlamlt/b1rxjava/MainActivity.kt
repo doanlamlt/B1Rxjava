6aec03f0838e7ab1d8a9c3d4a975b50d69d48cc8
Lam <doanlamlt@gmail.com>
2022-05-25 23:56:33 +0700

    Init comit

---
+++ app/src/main/java/com/dlamlt/b1rxjava/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dlamlt.b1rxjava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.reactivex.rxjava3.core.Observable
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Observable.fromArray("Lam","Huy","Trung")
+            .subscribe{onNext -> txtView.text}
+    }
+}
